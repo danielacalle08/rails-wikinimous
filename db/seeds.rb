0a70576aba134f55144ac9ee2f5d82621a474eea
Daniela Calle Meneses <danielacalle08@hotmail.com>
2022-09-10 23:49:32 +0200

    CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,11 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# #
+# movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+# Character.create(name: "Luke", movie: movies.first)
+puts 'Seeding data...'
+
+10.times do
+  Article.create(title: Faker::Book.title, content: Faker::Quote.matz)
+end
